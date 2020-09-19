a7c66e0ad845b340ed4e0f91bde1801d49381357
khetwal-om <khetwalom@gmail.com>
2020-09-19 23:07:34 +0530

    3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { applyMiddleware, createStore } from 'redux'
 import App from './App'
-import './index.css'
 import thunk from 'redux-thunk'
 import reducers from './reducers'
 
